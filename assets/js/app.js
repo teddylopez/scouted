f8997cd5be7ae29054ab7c38f4cf977da31bad00
Ted Lopez <>
2021-05-28 16:36:00 -0400

    Add clickable table row functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
 // its own CSS file.
 import "../css/app.scss"
 import 'alpinejs'
+import "./tables.js"
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
