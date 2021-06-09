7e2bde234892812e2a3f118a672b0b2d1225ee50
tedlopez <tedmlopez@gmail.com>
2021-06-09 15:01:58 -0400

    Deployment to Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,46 +10,22 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :scouted, ScoutedWeb.Endpoint,
-  # url: [host: "https://required-vacant-icefish.gigalixirapp.com", port: 80],
-  url: [host: "scouted.gigalixirapp.com", port: 443, scheme: "https"],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "scouted.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :scouted, ScoutedWeb.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-#         transport_options: [socket_opts: [:inet6]]
-#       ]
-#
-# The `cipher_suite` is set to `:strong` to support only the
-# latest and more secure SSL ciphers. This means old browsers
-# and clients may not be supported. You can set it to
-# `:compatible` for wider support.
-#
-# `:keyfile` and `:certfile` expect an absolute path to the key
-# and cert in disk or a relative path inside priv, for example
-# "priv/ssl/server.key". For all supported SSL configuration
-# options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-#
-# We also recommend setting `force_ssl` in your endpoint, ensuring
-# no data is ever sent via http, always redirecting to https:
-#
-# config :scouted, ScoutedWeb.Endpoint, force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+config :scouted, Scouted.Repo,
+  database: System.get_env("DATABASE_NAME"),
+  hostname: System.get_env("DATABASE_HOSTNAME"),
+  username: System.get_env("DATABASE_USER"),
+  password: System.get_env("DATABASE_PASSWORD"),
+  url: System.get_env("DATABASE_URL"),
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 18
 
-# Finally import the config/prod.secret.exs which loads secrets
-# and configuration from environment variables.
 import_config "prod.secret.exs"
--- lib/scouted_web/endpoint.ex
+++ lib/scouted_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ScoutedWeb.Endpoint do
   ]
 
   socket "/socket", ScoutedWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
